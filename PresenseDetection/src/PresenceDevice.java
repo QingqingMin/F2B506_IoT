34af2297c89e87baa78ea0e19a4ef887411f9532
MinQingyun <melissa.p.min@gmail.com>
2017-02-17 13:53:58 +0100

    Device Found

--- PresenseDetection/src/PresenceDevice.java
+++ PresenseDetection/src/PresenceDevice.java
@@ -38,25 +38,10 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 		
 		Argument powerArg = getPowerAction.getArgument("Power");
 		StateVariable powerState = powerArg.getRelatedStateVariable();
-		AllowedValueList allowList = powerState.getAllowedValueList();
-//		for (int n=0; n<allowList.size(); n++) 
-//			System.out.println("[" + n + "] = " + allowList.getAllowedValue(n));
-			
-		AllowedValueRange allowRange = powerState.getAllowedValueRange();
-//		System.out.println("maximum = " + allowRange.getMaximum());
-//		System.out.println("minimum = " + allowRange.getMinimum());
-//		System.out.println("step = " + allowRange.getStep());
+
 		
 		Argument presenceArg = getPresenceAction.getArgument("Presence");
 		StateVariable presenceState = presenceArg.getRelatedStateVariable();
-		AllowedValueList allowList_2 = presenceState.getAllowedValueList();
-//		for (int n=0; n<allowList_2.size(); n++) 
-//			System.out.println("[" + n + "] = " + allowList_2.getAllowedValue(n));
-			
-		AllowedValueRange allowRange_2 = presenceState.getAllowedValueRange();
-//		System.out.println("maximum = " + allowRange_2.getMaximum());
-//		System.out.println("minimum = " + allowRange_2.getMinimum());
-//		System.out.println("step = " + allowRange_2.getStep());
 
 	}
 
