1eb67e640f003766b1afb2a915e5038c3fcaa332
MinQingyun <melissa.p.min@gmail.com>
2017-02-17 11:56:41 +0100

    Third PUSH

--- PresenseDetection/src/PresenceDevice.java
+++ PresenseDetection/src/PresenceDevice.java
@@ -9,6 +9,7 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 	private final static String DESCRIPTION_FILE_NAME = "description/description.xml";
 
 	private StateVariable powerVar;
+	private StateVariable presenceVar;
 
 	public PresenceDevice() throws InvalidDescriptionException
 	{
@@ -20,11 +21,18 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 		Action setPowerAction = getAction("SetPower");
 		setPowerAction.setActionListener(this);
 		
+		Action getPresenceAction = getAction("GetPresence");
+		getPowerAction.setActionListener(this);
+		
+		Action setPresenceAction = getAction("SetPresence");
+		setPowerAction.setActionListener(this);
+		
 		ServiceList serviceList = getServiceList();
 		Service service = serviceList.getService(0);
 		service.setQueryListener(this);
 
 		powerVar = getStateVariable("Power");
+		presenceVar = getStateVariable("Presence");
 
 		Argument powerArg = getPowerAction.getArgument("Power");
 		StateVariable powerState = powerArg.getRelatedStateVariable();
@@ -36,6 +44,17 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 		System.out.println("maximum = " + allowRange.getMaximum());
 		System.out.println("minimum = " + allowRange.getMinimum());
 		System.out.println("step = " + allowRange.getStep());
+		
+		Argument presenceArg = getPresenceAction.getArgument("Presence");
+		StateVariable presenceState = presenceArg.getRelatedStateVariable();
+		AllowedValueList allowList_2 = presenceState.getAllowedValueList();
+		for (int n=0; n<allowList_2.size(); n++) 
+			System.out.println("[" + n + "] = " + allowList_2.getAllowedValue(n));
+			
+		AllowedValueRange allowRange_2 = presenceState.getAllowedValueRange();
+		System.out.println("maximum = " + allowRange_2.getMaximum());
+		System.out.println("minimum = " + allowRange_2.getMinimum());
+		System.out.println("step = " + allowRange_2.getStep());
 	}
 
 	////////////////////////////////////////////////
@@ -59,6 +78,7 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 	////////////////////////////////////////////////
 
 	private boolean onFlag = false;
+	private boolean presenceFlag = false;
 	
 	public void on()
 	{
@@ -75,6 +95,23 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 	public boolean isOn()
 	{
 		return onFlag;
+	}
+	
+	public void present()
+	{
+		presenceFlag = true;
+		presenceVar.setValue("yes");
+	}
+	
+	public void notpresent()
+	{
+		presenceFlag = false;
+		presenceVar.setValue("no");
+	}
+	
+	public boolean isPresent()
+	{
+		return presenceFlag;
 	}
 	
 	public void setPowerState(String state)
@@ -96,6 +133,29 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 	public String getPowerState()
 	{
 		if (onFlag == true)
+			return "1";
+		return "0";
+	}
+	
+	public void setPresenceState(String state)
+	{
+		if (state == null) {
+			notpresent();
+			return;
+		}
+		if (state.compareTo("1") == 0) {
+			present();
+			return;
+		}
+		if (state.compareTo("0") == 0) {
+			notpresent();
+			return;
+		}
+	}
+	
+	public String getPresenceState()
+	{
+		if (presenceFlag == true)
 			return "1";
 		return "0";
 	}
@@ -125,6 +185,21 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 			resultArg.setValue(state);
 			ret = true;
 		}
+		if (actionName.equals("GetPresence") == true) {
+			String state = getPresenceState();
+			Argument presenceArg = action.getArgument("Presence");
+			presenceArg.setValue(state);
+			ret = true;
+		}
+		if (actionName.equals("SetPresence") == true) {
+			Argument presenceArg = action.getArgument("Presence");
+			String state = presenceArg.getValue();
+			setPresenceState(state);
+			state = getPresenceState();
+			Argument resultArg = action.getArgument("Result");
+			resultArg.setValue(state);
+			ret = true;
+		}
 
 		comp.repaint();
 
@@ -138,6 +213,7 @@ public class PresenceDevice extends Device implements ActionListener, QueryListe
 	public boolean queryControlReceived(StateVariable stateVar)
 	{
 		stateVar.setValue(getPowerState());
+		stateVar.setValue(getPresenceState());
 		return true;
 	}
 
